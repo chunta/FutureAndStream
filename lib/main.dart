255ac4cefedc7e97d9fd8781e6be97a873e2b63e
Rex Chen <rexchen@RexdeMac-mini.local>
2024-03-07 18:44:53 +0800

    add client networking in debug

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
 import 'dart:async';
-import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -52,34 +51,12 @@ Future<Album> fetchAlbum() async {
 }
 
 void main() {
-  test();
   runApp(const MyApp());
 }
 
 void test() async {
-  /*
-  await for (final name in femaleNames().map((name) => name.toUpperCase())) {
-    print(name);
-  }
-
-  await for (final name in maleNames().testccap) {
-    print(name);
-  }
-
-  print("====");
-  await for (final name in maleNames().aabb()) {
-    print(name);
-  }
-
-  await for (final name in maleNames().aabbs()) {
-    print(name);
-  }
-
-  print("====");
-
   final allall = await maleNames().toList();
   print(allall);
-
   final result = await getTmpNames()
       .asyncMap((name) => extractCharacters(name))
       .fold('', (previous, element) {
@@ -87,18 +64,15 @@ void test() async {
     return '$previous $elements';
   });
 
-  print(result);
-
   final threeTimes = getTmpNames().asyncExpand((event) => times3(event));
   await for (final name in threeTimes) {
-    print(name);
+    (name);
   }
-  */
 
-  print(2.doubleIt());
   final controller = StreamController<String>.broadcast();
+  // ignore: unused_local_variable
   final sub1 = controller.stream.listen((event) {
-    print(event);
+    print(" line 76 ${event}");
   });
 
   controller.sink.add("1");
@@ -163,23 +137,29 @@ Stream<String> femaleNames() async* {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    test();
     return Center(
       child: FutureBuilder<Album>(
           future: fetchAlbum(),
           builder: (BuildContext context, AsyncSnapshot snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasError) {
+                return const Text(
+                  "Error",
+                  textDirection: TextDirection.ltr,
+                );
+              }
               return RichText(
                 text: TextSpan(
                   text: '~~~~${snapshot.data!.title}',
-                  style: TextStyle(fontWeight: FontWeight.bold),
+                  style: const TextStyle(fontWeight: FontWeight.bold),
                 ),
                 textDirection: TextDirection.ltr,
               );
             } else {
-              return CircularProgressIndicator();
+              return const CircularProgressIndicator();
             }
           }),
     );
